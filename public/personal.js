9dbac74fd6192fc4a9a63c1b6b2bacb2e961774f
Gurleen Khatra <grlnk97@gmail.com>
2021-04-26 21:23:55 -0500

    added database path

--- public/personal.js
+++ public/personal.js
@@ -8,10 +8,11 @@ function openTab(personal){
 }
 
 // TODO: Replace the following with your app's Firebase project configuration
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyC-z98Nc4j11cPpC6eRqB3iQOSlQrfe7aA",
     authDomain: "comp430-764fa.firebaseapp.com",
+    databaseURL: "https://comp430-764fa-default-rtdb.firebaseio.com",
     projectId: "comp430-764fa",
     storageBucket: "comp430-764fa.appspot.com",
     messagingSenderId: "774865356524",
